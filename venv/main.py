9356c6248d7cc202750dd978cd0aee0ba7e94a5b
temmyy <temmy.work.free@gmail.com>
2020-12-15 10:12:19 +0700

    с первичной раздачей двух карт

--- venv/main.py
+++ venv/main.py
@@ -10,13 +10,22 @@ if scriptType == 1:
     while again:
         deck = [6, 7, 8, 9, 10, 2, 3, 4, 11] * 4
         currentBalance = 0
+        random.shuffle(deck)
         while True:
+
+            if currentBalance == 0:
+                k1 = deck.pop()
+                k2 = deck.pop()
+                print('\n'
+                      'ваша раздача: %d + %d' % (k1, k2))
+                currentBalance = k1 + k2
+                print('вы набрали %d' % currentBalance)
+
             if input("будете брать карту? ") == "д":
-                random.shuffle(deck)
                 currentCard = deck.pop()
                 currentBalance += currentCard
-                print('вам выпало %d' %currentCard)
-                print('вы набрали %d' %currentBalance)
+                print('вам выпало %d' % currentCard)
+                print('вы набрали %d' % currentBalance)
                 if currentBalance == 21:
                     print('вы победили :)')
                     break
